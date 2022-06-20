eca3daa26191701048b2677f6a408645708b7a4b
mohit199412 <52268808+mohit199412@users.noreply.github.com>
2022-06-20 14:57:20 -0400

    change

--- com.tf
+++ com.tf
@@ -1,7 +1,6 @@
 resource "google_compute_instance" "default"{
   name="test-instance"
   machine_type= "f1-micro"
-  zone ="us-central1-a"
   boot_disk {
 	initialize_params{
 	image ="debian-cloud/debian-9"
--- project.tf
+++ project.tf
@@ -1,6 +1,5 @@
 provider "google" {
   credentials = "/home/terraform/key/key.json"
-  region      = "us-central1"
   project     ="spring-banner-353911"
 }
 
