bce3fefab4217eca054ee697d913c5638f6af718
mohit199412 <52268808+mohit199412@users.noreply.github.com>
2022-06-21 12:55:52 +0530

    update zone

--- com.tf
+++ com.tf
@@ -2,7 +2,7 @@
 resource "google_compute_instance" "default" {
   name         = "flask-vm"
   machine_type = "f1-micro"
-  zone         = "us-west1-a"
+  zone         = "us-central1-a"
   tags         = ["ssh"]
 
   metadata = {
